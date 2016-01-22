e559efeee84420afc3b80e68304e358659526406
Carlos Pienovi <carlospienovi@gmail.com>
2016-01-22 10:13:10 -0300

    refactoring g plus login and added api 23 permission support

--- library/src/main/java/io/github/fanky10/sociallogin/module/fragments/BaseGooglePlusLoginFragment.java
+++ library/src/main/java/io/github/fanky10/sociallogin/module/fragments/BaseGooglePlusLoginFragment.java
@@ -1,28 +1,67 @@
 package io.github.fanky10.sociallogin.module.fragments;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.People;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 
+import java.io.IOException;
+
+import io.github.fanky10.sociallogin.module.constants.SocialLoginConstants;
 import io.github.fanky10.sociallogin.module.interfaces.ISocialLogin;
-import io.github.fanky10.sociallogin.module.providers.GooglePlusProvider;
+
 
 /**
  * Created by carlospienovi1 on 12/30/15.
  */
 public abstract class BaseGooglePlusLoginFragment extends Fragment implements
-        ISocialLogin {
+        ISocialLogin,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<People.LoadPeopleResult> {
 
-    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    private static final int REQUEST_GET_ACCOUNTS_PERMISSION = 102;
+    public static final int REQUEST_RESOLVE_ERROR = 1001;
 
-    private GooglePlusProvider mGooglePlusProvider;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .addScope(new Scope(Scopes.EMAIL))
+                .build();
+    }
 
     public void doGoogleLogin() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.GET_ACCOUNTS}, REQUEST_CODE_ASK_PERMISSIONS);
+                requestPermissions(new String[]{Manifest.permission.GET_ACCOUNTS}, REQUEST_GET_ACCOUNTS_PERMISSION);
             } else {
                 doGoogleLoginProcess();
             }
@@ -32,14 +71,84 @@ public abstract class BaseGooglePlusLoginFragment extends Fragment implements
     }
 
     private void doGoogleLoginProcess() {
-        mGooglePlusProvider = new GooglePlusProvider(getActivity());
-        mGooglePlusProvider.setCallback(this);
-
-        if (mGooglePlusProvider != null) {
-            if (!mGooglePlusProvider.isConnected()) {
-                mGooglePlusProvider.connectApiClient();
+        if (mGoogleApiClient != null) {
+            if (!mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.connect();
             }
-            mGooglePlusProvider.setUpSignIn();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Plus.PeopleApi.load(mGoogleApiClient, "signed_in_user_account_id").setResultCallback(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        onSocialProviderConnectionCanceled();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(getActivity(), REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            onSocialProviderConnectionFailure(new Exception(connectionResult.getErrorMessage()));
+        }
+    }
+
+    @Override
+    public void onResult(@NonNull People.LoadPeopleResult loadPeopleResult) {
+        final Person person = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+        final String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+        new AsyncTask<String, Void, String>() {
+
+            @Override
+            protected String doInBackground(String... email) {
+                try {
+                    return GoogleAuthUtil.getToken(getContext(), email[0], "oauth2:profile email");
+                } catch (IOException | GoogleAuthException ignored) {
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String token) {
+                if (token != null) {
+                    JSONObject json = new JSONObject();
+                    try {
+                        json.put(SocialLoginConstants.GOOGLE_FIRST_NAME, person.getName().getGivenName());
+                        json.put(SocialLoginConstants.GOOGLE_LAST_NAME, person.getName().getFamilyName());
+                        json.put(SocialLoginConstants.GOOGLE_EMAIL, email);
+                    } catch (JSONException ignore) {
+
+                    }
+                    onSocialProviderConnected(token, json);
+                } else {
+                    onSocialProviderConnectionFailure(new Exception("generic error"));
+                }
+            }
+
+        }.execute(email);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_GET_ACCOUNTS_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                doGoogleLoginProcess();
+            } else {
+                onSocialProviderConnectionFailure(new Exception("GET ACCOUNTS permission was not granted"));
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
